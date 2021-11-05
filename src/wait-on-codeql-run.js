5591addf2c31ef6b4b2a95a7ef8b8ca8b289c3ca
Rob Bos <raj.bos@gmail.com>
2021-11-05 21:23:31 +0100

    huh

--- src/wait-on-codeql-run.js
+++ src/wait-on-codeql-run.js
@@ -5,14 +5,16 @@ module.exports = async ({github, owner, repo}) => {
     let data
     try {
         console.log(`before the call`)
-        let { data } = await github.rest.actions.listWorkflowRunsForRepo({ 
+        let data  = await github.rest.actions.listWorkflowRunsForRepo({ 
             owner,
             repo
         })
         console.log(`after the call`)
 
         console.log(`data:`)
-        console.log(data)        
+        console.log(data)      
+        console.log(`data.content:`)
+        console.log(data.content)
     }
     catch (e) {
         // strange: seems like we are not getting here at all :-(
