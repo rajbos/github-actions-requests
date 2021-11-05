4b76a39593fb903bbd37c6c031dd3ce07238b6a0
Rob Bos <raj.bos@gmail.com>
2021-11-05 21:30:52 +0100

    skipping try catch

--- src/wait-on-codeql-run.js
+++ src/wait-on-codeql-run.js
@@ -2,28 +2,15 @@ module.exports = async ({github, owner, repo}) => {
 
     console.log(`Looking at this repository: [${owner}/${repo}]`)
 
-    let data
-    try {
-        console.log(`before the call`)
-        let data  = await github.rest.actions.listWorkflowRunsForRepo({ 
-            owner,
-            repo
-        })
-        console.log(`after the call`)
+    console.log(`before the call`)
+    let { data } = await github.rest.actions.listWorkflowRunsForRepo({ 
+        owner,
+        repo
+    })
 
-        console.log(`data:`)
-        console.log(data)      
-        console.log(`data.content:`)
-        console.log(data.content)
-    }
-    catch (e) {
-        // strange: seems like we are not getting here at all :-(
-        console.log(`in the catch with e:`)
-        console.log(e)
-    }
-    console.log(`after the catch`)
     console.log(`data:`)
-    console.log(data)    
+    console.log(data)        
+
     if (data === undefined) {
         console.log(`data is undefined`)
         return 1
