237a48b9b8ce667f3d1b2daddd2215a5094b7f61
Rob Bos <raj.bos@gmail.com>
2021-11-05 21:13:04 +0100

    Testing workflow runs and triggering a new one

--- src/wait-on-codeql-run.js
+++ src/wait-on-codeql-run.js
@@ -2,6 +2,7 @@ module.exports = async ({github, owner, repo}) => {
 
     console.log(`Looking at this repository: [${owner}/${repo}]`)
 
+    let data
     try {
         console.log(`before the call`)
         let { data } = await github.rest.actions.listWorkflowRunsForRepo({ 
@@ -23,3 +24,24 @@ module.exports = async ({github, owner, repo}) => {
     }
     console.log(`after the catch`)
 }
+
+if (data === undefined) {
+    return 1
+}
+
+codeqlRuns = data.workflow_runs.filter(run => run.name === 'CodeQL')
+console.log(`codeqlRuns: ${JSON.stringify(codeqlRuns)}`)
+
+// get the most recent one, is always on top:
+codeQLRun = codeqlRuns[0]
+console.log(`codeQLRun info: id: [${codeQLRun.id}], status: : [${codeQLRun.status}], created_at: [${codeQLRun.created_at}], conclusion: : [${codeQLRun.conclusion}], workflow_id: [${codeqlRun.workflow_id}]`)
+
+// manually dispatch the workflow again, so that we can wait for it
+let { data } = await github.rest.actions.createWorkflowDispatch({
+    owner,
+    repo,
+    workflow_id: codeqlRun.workflow_id,
+    ref: codeqlRun.head_branch,
+  });
+
+  console.log(`Start workflow result data: [${JSON.stringify(data)}]`)
