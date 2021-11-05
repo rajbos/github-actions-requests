4731efbff03be99de24256cbf42b4cd21000f149
Rob Bos <raj.bos@gmail.com>
2021-11-05 21:36:10 +0100

    casing matters!

--- src/wait-on-codeql-run.js
+++ src/wait-on-codeql-run.js
@@ -20,14 +20,14 @@ module.exports = async ({github, owner, repo}) => {
 
     // get the most recent one, is always on top:
     codeQLRun = codeqlRuns[0]
-    console.log(`codeQLRun info: id: [${codeQLRun.id}], status: : [${codeQLRun.status}], created_at: [${codeQLRun.created_at}], conclusion: : [${codeQLRun.conclusion}], workflow_id: [${codeQLRun.workflow_id}]`)
+    console.log(`codeQLRun info: id: [${codeQLRun.id}], status: [${codeQLRun.status}], created_at: [${codeQLRun.created_at}], conclusion: [${codeQLRun.conclusion}], workflow_id: [${codeQLRun.workflow_id}]`)
 
     // manually dispatch the workflow again, so that we can wait for it
     let { dispatchData } = await github.rest.actions.createWorkflowDispatch({
         owner,
         repo,
-        workflow_id: codeqlRun.workflow_id,
-        ref: codeqlRun.head_branch,
+        workflow_id: codeQLRun.workflow_id,
+        ref: codeQLRun.head_branch,
     });
 
     console.log(`Start workflow result data: [${JSON.stringify(dispatchData)}]`)
