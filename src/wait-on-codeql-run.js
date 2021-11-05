62dbbe3cee3b3fdaf76cb7ce41d4260d7c595265
Rob Bos <raj.bos@gmail.com>
2021-11-05 21:15:58 +0100

    fix indentation

--- src/wait-on-codeql-run.js
+++ src/wait-on-codeql-run.js
@@ -23,25 +23,26 @@ module.exports = async ({github, owner, repo}) => {
         console.log(e)
     }
     console.log(`after the catch`)
+
+    if (data === undefined) {
+        return 1
+    }
+
+    codeqlRuns = data.workflow_runs.filter(run => run.name === 'CodeQL')
+    console.log(`codeqlRuns: ${JSON.stringify(codeqlRuns)}`)
+
+    // get the most recent one, is always on top:
+    codeQLRun = codeqlRuns[0]
+    console.log(`codeQLRun info: id: [${codeQLRun.id}], status: : [${codeQLRun.status}], created_at: [${codeQLRun.created_at}], conclusion: : [${codeQLRun.conclusion}], workflow_id: [${codeqlRun.workflow_id}]`)
+
+    // manually dispatch the workflow again, so that we can wait for it
+    let { data } = await github.rest.actions.createWorkflowDispatch({
+        owner,
+        repo,
+        workflow_id: codeqlRun.workflow_id,
+        ref: codeqlRun.head_branch,
+    });
+
+    console.log(`Start workflow result data: [${JSON.stringify(data)}]`)
+
 }
-
-if (data === undefined) {
-    return 1
-}
-
-codeqlRuns = data.workflow_runs.filter(run => run.name === 'CodeQL')
-console.log(`codeqlRuns: ${JSON.stringify(codeqlRuns)}`)
-
-// get the most recent one, is always on top:
-codeQLRun = codeqlRuns[0]
-console.log(`codeQLRun info: id: [${codeQLRun.id}], status: : [${codeQLRun.status}], created_at: [${codeQLRun.created_at}], conclusion: : [${codeQLRun.conclusion}], workflow_id: [${codeqlRun.workflow_id}]`)
-
-// manually dispatch the workflow again, so that we can wait for it
-let { data } = await github.rest.actions.createWorkflowDispatch({
-    owner,
-    repo,
-    workflow_id: codeqlRun.workflow_id,
-    ref: codeqlRun.head_branch,
-  });
-
-  console.log(`Start workflow result data: [${JSON.stringify(data)}]`)
