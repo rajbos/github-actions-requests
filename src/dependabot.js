b3235f15d2ba0f0c33fc60e2ad64e7f93aa7a8a8
Rob Bos <raj.bos@gmail.com>
2021-11-19 15:27:26 +0100

    using map instead of distinct

--- src/dependabot.js
+++ src/dependabot.js
@@ -28,8 +28,8 @@ module.exports = async ({github, owner, repo}) => {
     });
     
     console.log(`Found ofa total of [${repository.vulnerabilityAlerts.nodes.length}] vulnerability alerts (contains duplicates)`)
-    const highAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH').distinct('securityVulnerability.package.name')
-    const moderateAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'MODERATE').distinct('securityVulnerability.package.name')
+    const highAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH').map(alert => alert.securityVulnerability.package.name)
+    const moderateAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'MODERATE').map(alert => alert.securityVulnerability.package.name)
 
     console.log(`Found [${highAlerts.length}] advisory with severity 'HIGH'`)
     console.log(`Found [${moderateAlerts.length}] advisory with severity 'MODERATE'`)
