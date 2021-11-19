d471c48c00fad601d1e05891c94d6f4b7a1875f2
Rob Bos <raj.bos@gmail.com>
2021-11-19 13:39:34 +0100

    Testing parameterized query

--- src/dependabot.js
+++ src/dependabot.js
@@ -2,15 +2,32 @@ module.exports = async ({github, owner, repo}) => {
     console.log(`Looking at this repository: [${owner}/${repo}]`)
 
     const {
-        viewer: { login },
+        result ,
       } = await github.graphql(`{
-        viewer {
-          login
+        repository($owner: String!, $repo: String!") {
+            vulnerabilityAlerts(first: 100) {
+                nodes {
+                    createdAt
+                    dismissedAt
+                    securityVulnerability {
+                        package {
+                            name
+                        }
+                        advisory {
+                            description
+                            severity
+                        }
+                    }
+                }
+            }
         }
-      }`);
+    }`,
+    {
+        owner,
+        repo,
+    });
       
 
-    console.log(`Repository result: ${login}`)
-      
+    console.log(`Repository result: ${result}`)      
 
 }
