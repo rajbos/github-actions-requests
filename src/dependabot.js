2491c7a4332d0461f62c205e19f382ee7344ea0d
Rob Bos <raj.bos@gmail.com>
2021-11-19 15:40:47 +0100

    create new set with the results to get an actual distinct array

--- src/dependabot.js
+++ src/dependabot.js
@@ -28,12 +28,12 @@ module.exports = async ({github, owner, repo}) => {
     });
     
     console.log(`Found a total of [${repository.vulnerabilityAlerts.nodes.length}] vulnerability alerts (contains duplicates)`)
-    const highAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH').map(alert => alert.securityVulnerability.package.name)
-    const moderateAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'MODERATE').map(alert => alert.securityVulnerability.package.name)
+    const highAlerts = [ ... new Set(repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH').map(alert => alert.securityVulnerability.package.name))]
+    const moderateAlerts = [ ... new Set(repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'MODERATE').map(alert => alert.securityVulnerability.package.name))]
 
     console.log(`Found [${highAlerts.length}] advisory with severity 'HIGH'`)
     console.log(`Found [${moderateAlerts.length}] advisory with severity 'MODERATE'`)
-    
+
     console.log(`high: ${JSON.stringify(highAlerts)}`)
 
     return {high: highAlerts.length,  moderate: moderateAlerts.length}
