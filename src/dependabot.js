82f62900d2d75d4129ac1a7b1fd55417e5c2d997
Rob Bos <raj.bos@gmail.com>
2021-11-19 15:36:49 +0100

    more logging

--- src/dependabot.js
+++ src/dependabot.js
@@ -27,12 +27,13 @@ module.exports = async ({github, owner, repo}) => {
         name: repo,
     });
     
-    console.log(`Found ofa total of [${repository.vulnerabilityAlerts.nodes.length}] vulnerability alerts (contains duplicates)`)
+    console.log(`Found a total of [${repository.vulnerabilityAlerts.nodes.length}] vulnerability alerts (contains duplicates)`)
     const highAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH').map(alert => alert.securityVulnerability.package.name)
     const moderateAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'MODERATE').map(alert => alert.securityVulnerability.package.name)
 
     console.log(`Found [${highAlerts.length}] advisory with severity 'HIGH'`)
     console.log(`Found [${moderateAlerts.length}] advisory with severity 'MODERATE'`)
+    console.log(`moderate: ${JSON.stringify(moderateAlerts)}`)
 
     return {high: highAlerts.length,  moderate: moderateAlerts.length}
 }
