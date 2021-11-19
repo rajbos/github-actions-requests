f19933f97489c01a3fbec48f1cf115c5f101625d
Rob Bos <raj.bos@gmail.com>
2021-11-19 14:57:15 +0100

    show medium as well

--- src/dependabot.js
+++ src/dependabot.js
@@ -30,7 +30,9 @@ module.exports = async ({github, owner, repo}) => {
     console.log(`Repository result: ${JSON.stringify(repository.vulnerabilityAlerts.nodes)}`)
     console.log(`-----------------------------------------------`)      
     console.log(`Count of the result: ${repository.vulnerabilityAlerts.nodes.length}`)
-    const alerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH')
+    const highAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'HIGH')
+    const mediumAlerts = repository.vulnerabilityAlerts.nodes.filter(alert => alert.securityVulnerability.advisory.severity === 'MEDIUM')
 
-    console.log(`Found [${alerts}] advisory with severity 'HIGH'`)
+    console.log(`Found [${highAlerts.length}] advisory with severity 'HIGH'`)
+    console.log(`Found [${mediumAlerts.length}] advisory with severity 'MEDIUM'`)
 }
