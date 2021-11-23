85e8b77346e1ac047943cf6d02f6eb852c996308
Rob Bos <raj.bos@gmail.com>
2021-11-23 17:22:56 +0100

    Adding info to the logs

--- src/get-codeql-results.js
+++ src/get-codeql-results.js
@@ -7,6 +7,7 @@ module.exports = async ({github, owner, repo}) => {
         repo,
     })
 
+    let scanResult
     if (recentScansData === undefined) {
         console.log(`recentScansData is undefined`)
     }
@@ -15,6 +16,11 @@ module.exports = async ({github, owner, repo}) => {
         // todo: check if the scan had any errors and act on it
         const mostRecentScan = recentScansData.data[0]
         console.log(`Found most recent scan data that was created at [${mostRecentScan.created_at}] that can be found at [${mostRecentScan.url}]. The scan had [${mostRecentScan.results_count}] results and these errors: [${mostRecentScan.error}]. The scan ran with these environment values: [${JSON.stringify(mostRecentScan.environment)}]`)
+        scanResult.created_at = mostRecentScan.created_at
+        scanResult.url = mostRecentScan.url
+        scanResult.results_count = mostRecentScan.results_count
+        scanResult.error = mostRecentScan.error
+        scanResult.environment = mostRecentScan.environment
     }
 
     // list all alerts, not just the most recent scan:
@@ -34,8 +40,11 @@ module.exports = async ({github, owner, repo}) => {
             console.log(`Overall check: Found [${data.length}] code scanning alerts for the repo`)
             // todo: check the severity of each alert / group by severity
             for (const alert of data) {
-            console.log(`Alert [${alert.number}], created at: [${alert.created_at}], state: [${alert.state}], dismissed: [${alert.dismissed}], severity: [${(alert.rule.security_severity_level)}] with description: [${(alert.rule.description)}]`)
+                console.log(`Alert [${alert.number}], created at: [${alert.created_at}], state: [${alert.state}], dismissed: [${alert.dismissed}], severity: [${(alert.rule.security_severity_level)}] with description: [${(alert.rule.description)}]`)
+                // todo: add the alert info to the result
             }
         }
     }
+
+    return scanResult
 }
