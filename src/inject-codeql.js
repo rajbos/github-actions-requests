257a46b6cf5fd31cfb5795ab8fad83569010ca42
Rob Bos <raj.bos@gmail.com>
2021-11-23 12:10:21 +0100

    Cleanup

--- src/inject-codeql.js
+++ src/inject-codeql.js
@@ -6,54 +6,15 @@ module.exports = async ({github, owner, repo}) => {
         const path = 'codeql-analysis.yml'
         const content = readFileSync(`${process.env.GITHUB_WORKSPACE}/${path}`)
         
-        const targetPath = ".github/workflows/codeql-analysis.yml"
-        console.log(`Checking to see if [${targetPath}] already exists in the forked repo`)
-        
-        // this call gives a 404, since the file does not exist, how to handle this?
-        let data
-        try {
-            console.log(`before the call`)
-            let { data } = await github.rest.repos.getContent({ 
-                owner,
-                repo,
-                path: ".github/workflows/codeql-analysis.yml"  // todo: use variable here
-            })
-            console.log(`after the call`)
-
-            console.log(`data:`)
-            console.log(data)
-            
-            console.log(`data.content:`)
-            console.log(data.content)
-        }
-        catch (e) {
-            // strange: seems like we are not getting here at all :-(
-            console.log(`in the catch with e:`)
-            console.log(e)
-        }
-        console.log(`after the catch`)
-        
-        let content2
-        if (data === undefined || data.content === undefined) {
-            console.log('data is null, so workflow does not exist yet')
-            content2 = content
-        } else {
-            console.log('data is not null, so does exist? We need to compare their hashes to see if we need to overwrite')
-            content2 = Buffer.from(data.content.toString('base64'), 'base64')
-            if (Buffer.from(data.content, 'base64').compare(content2) === 0) {
-            console.log('data in the codeql workflow file is the same. Halting execution')
-            return
-            }
-        }
-                    
+        const targetPath = ".github/workflows/codeql-analysis.yml"                                    
         console.log(`Uploading the CodeQL workflow to the forked repository`)
         github.rest.repos.createOrUpdateFileContents({
             owner,
             repo,
             path: targetPath,
-            message: "Adding CodeQL setup",
-            content: content2.toString('base64'),
-            sha: data === undefined ? undefined : data.sha
+            message: "🤖 Adding CodeQL workflow file",
+            content: content.toString('base64'),
+            sha: undefined
         })
 
         console.log('CodeQL workflow uploaded')
