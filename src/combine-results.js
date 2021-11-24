b8d8d2f862194034db6a661921a3f38a746a2ada
Rob Bos <raj.bos@gmail.com>
2021-11-24 16:06:48 +0100

    Moving code to separate js file

---
+++ src/combine-results.js
@@ -0,0 +1,38 @@
+module.exports = async ({github, owner, repo, issue_number, codeqlResult}) => {
+
+    console.log(``)
+    console.log(`Looking at this repository: [${owner}/${repo}] with issue number [${issue_number}]`)
+
+    const codeqlInfo = JSON.parse(codeqlResult.substring(1, codeql.length - 1))
+    console.log(`CodeQL scan results:`)
+    console.log(`- url: ${codeqlInfo.url}`)
+    console.log(`- results.count: [${codeqlInfo.results_count}]`)
+    console.log(`- environment: [${codeqlInfo.environment}]`)
+    console.log(`- created_at: [${codeqlInfo.created_at}]`)
+
+    let codeQLSymbol = ''
+    if (codeqlInfo.results_count === 0) {
+        codeQLSymbol = ':white_check_mark:'
+    }
+    else {
+        codeQLSymbol = ':x:'
+    }
+
+    let commentBody = [
+        "Found these results:",
+        "",
+        "|Check|Results|",
+        "|---|---|",
+        '|CodeQL on the forked repo|${codeQLSymbol}|',
+        "",
+        ":robot:"
+      ]
+      
+      // create comment letting the user know the results
+      github.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number,
+        body: commentBody.join('\n')
+      });
+}  
