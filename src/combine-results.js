af31921953c438e54ae8bb1f26e3ae406135a446
Rob Bos <raj.bos@gmail.com>
2021-11-24 16:36:43 +0100

    Is it already a full object then?

--- src/combine-results.js
+++ src/combine-results.js
@@ -6,8 +6,14 @@ module.exports = async ({github, owner, repo, issue_number, codeqlResult}) => {
     console.log(`Has object type: [${typeof codeqlResult}]`)
     console.log(`Has value: [${JSON.stringify(codeqlResult)}]`)
 
-    const codeqlInfo = JSON.parse(codeqlResult.substring(1, codeqlResult.length - 1))
     console.log(`CodeQL scan results:`)
+    console.log(`- url: ${codeqlResult.url}`)
+    console.log(`- results.count: [${codeqlResult.results_count}]`)
+    console.log(`- environment: [${codeqlResult.environment}]`)
+    console.log(`- created_at: [${codeqlResult.created_at}]`)
+
+    const codeqlInfo = JSON.parse(codeqlResult.substring(1, codeqlResult.length - 1))
+    console.log(`CodeQL scan results 2:`)
     console.log(`- url: ${codeqlInfo.url}`)
     console.log(`- results.count: [${codeqlInfo.results_count}]`)
     console.log(`- environment: [${codeqlInfo.environment}]`)
