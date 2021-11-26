fd75ba7a571f62a910612f09da04b1117449488e
Rob Bos <raj.bos@gmail.com>
2021-11-26 16:09:21 +0100

    Moved icons to second column

--- src/update-issue-comment.sh
+++ src/update-issue-comment.sh
@@ -2,48 +2,48 @@
 
 set -euo pipefail
 
-echo "| Result | Step |" > result.md
+echo "| Check | Result |" > result.md
 echo "|---|---|" >> result.md
 
 if [ $HAS_GITHUB_FOLDER == false  ]; then
-    echo "| ⛔️ | No .github folder found |" >> result.md    
+    echo "| No .github folder found | ⛔️ |" >> result.md    
 fi
 
 if [ $HAS_WORKFLOWS_FOLDER == false ]; then
-    echo "| ⛔️ | No Workflows found  |" >> result.md
+    echo "| No Workflows found | ⛔️ |" >> result.md
 fi
 
 if [ $HAS_DEPENDABOT_CONFIGURATION == true ]; then
-    echo "| ✅ | Dependabot configuration found |" >> result.md
+    echo "| Dependabot configuration found | ✅ |" >> result.md
 else
-    echo "| ⛔️ | No Dependabot configuration found |" >> result.md
+    echo "| No Dependabot configuration found | ⛔️ |" >> result.md
 fi
 
 if [ $HAS_CODEQL_INIT == true ]; then
-    echo "| ✅ | CodeQL Init found in $WORKFLOW_WITH_CODEQL_INIT |" >> result.md
+    echo "| CodeQL Init found in $WORKFLOW_WITH_CODEQL_INIT | ✅ |" >> result.md
 else
-    echo "| ⛔️ | No CodeQL Init found |" >> result.md
+    echo "| No CodeQL Init found | ⛔️ |" >> result.md
 fi
 
 if [ $HAS_CODEQL_ANALYZE == true ]; then
-    echo "| ✅ | CodeQL Analyze found in $WORKFLOW_WITH_CODEQL_ANALYZE |" >> result.md
+    echo "| CodeQL Analyze found in $WORKFLOW_WITH_CODEQL_ANALYZE | ✅ |" >> result.md
 else
-    echo "| ⛔️ | No CodeQL Analyze found |" >> result.md
+    echo "| No CodeQL Analyze found | ⛔️ |" >> result.md
 fi
 
 if [ $ACTION_USES_DOCKER == true ]; then
     echo "| ℹ️ | Uses Docker container |" >> result.md
 
     if [ $HAS_LOW_MEDIUM_ISSUES == true ]; then
-        echo "| ⚠️ | $LOW_MEDIUM_ISSUES Low or medium issues found |" >> result.md
+        echo "| $LOW_MEDIUM_ISSUES Low or medium issues found | ⚠️ |" >> result.md
     else
-        echo "| ✅ | No Low or Medium issues found |" >> result.md
+        echo "| No Low or Medium issues found | ✅ |" >> result.md
     fi
 
     if [ $HAS_HIGH_CRITICAL_ISSUES == true ]; then
-        echo "| ⛔️ | $HIGH_CRITICAL_ISSUES High or critical issues found |" >> result.md
+        echo "| $HIGH_CRITICAL_ISSUES High or critical issues found | ⛔️ |" >> result.md
     else
-        echo "| ✅ | No High or Critical issues found |" >> result.md
+        echo "| ✅ | No High or Critical issues found | ✅ |" >> result.md
     fi
 fi
 
