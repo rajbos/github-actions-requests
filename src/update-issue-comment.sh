db7aeab1b1feefeac532720ec84b7d8e9b3c4412
Rob Bos <raj.bos@gmail.com>
2021-11-26 15:58:50 +0100

    Change all ifs

--- src/update-issue-comment.sh
+++ src/update-issue-comment.sh
@@ -2,11 +2,6 @@
 
 set -euo pipefail
 
-echo "HAS_CODEQL_INIT: $HAS_CODEQL_INIT"
-echo "HAS_CODEQL_ANALYZE: $HAS_CODEQL_ANALYZE"
-echo "HAS_GITHUB_FOLDER: $HAS_GITHUB_FOLDER"
-echo ""
-
 echo "| Result | Step |" > result.md
 echo "|---|---|" >> result.md
 
@@ -18,7 +13,7 @@ if [ $HAS_WORKFLOWS_FOLDER == false ]; then
     echo "| ⛔️ | No Workflows found  |" >> result.md
 fi
 
-if [ $HAS_DEPENDABOT_CONFIGURATION ]; then
+if [ $HAS_DEPENDABOT_CONFIGURATION == true ]; then
     echo "| ✅ | Dependabot configuration found |" >> result.md
 else
     echo "| ⛔️ | No Dependabot configuration found |" >> result.md
@@ -30,22 +25,22 @@ else
     echo "| ⛔️ | No CodeQL Init found |" >> result.md
 fi
 
-if [ $HAS_CODEQL_ANALYZE ]; then
+if [ $HAS_CODEQL_ANALYZE == true ]; then
     echo "| ✅ | CodeQL Analyze found in $WORKFLOW_WITH_CODEQL_ANALYZE |" >> result.md
 else
     echo "| ⛔️ | No CodeQL Analyze found |" >> result.md
 fi
 
-if [ $ACTION_USES_DOCKER ]; then
+if [ $ACTION_USES_DOCKER == true ]; then
     echo "| ℹ️ | Uses Docker container |" >> result.md
 
-    if [ $HAS_LOW_MEDIUM_ISSUES ]; then
+    if [ $HAS_LOW_MEDIUM_ISSUES == true ]; then
         echo "| ⚠️ | $LOW_MEDIUM_ISSUES Low or medium issues found |" >> result.md
     else
         echo "| ✅ | No Low or Medium issues found |" >> result.md
     fi
 
-    if [ $HAS_HIGH_CRITICAL_ISSUES ]; then
+    if [ $HAS_HIGH_CRITICAL_ISSUES == true ]; then
         echo "| ⛔️ | $HIGH_CRITICAL_ISSUES High or critical issues found |" >> result.md
     else
         echo "| ✅ | No High or Critical issues found |" >> result.md
