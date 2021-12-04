523a8270dc2f53cf7f774ee8e435953b3c50c5ac
Rob Bos <raj.bos@gmail.com>
2021-12-04 20:04:52 +0100

    Adding more log info

--- src/security-scan.sh
+++ src/security-scan.sh
@@ -99,7 +99,7 @@ function action_docker_checks() {
 
     # Check if HIGH or CRITICAL issues are found (remove count from header)
     HIGH_CRITICAL_ISSUES=$(cat issues | grep -e HIGH -e CRITICAL | wc -l)
-    echo " - $HIGH_CRITICAL_ISSUES low and medium issues found"
+    echo " - $HIGH_CRITICAL_ISSUES high and crititcal issues found"
     
     if [ $HIGH_CRITICAL_ISSUES -gt 0 ] ; then
         echo ::set-output name=high_critical_issues::$HIGH_CRITICAL_ISSUES
